1701e6683f24da3fd45884d3fc8912ba38b78dba
ErnestMaina <you@example.com>
2023-09-18 08:05:00 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee={
+    address:{
+        Mark:"2345 Ngong",
+        Jane:"5839 Town",
+        Josh:"9998 Kilimani"
+    }
+}
+
+employee.address.Josh="777 Minsk"
+
+
+console.log(employee.address.Josh)
+
+delete employee.address.Jane;
+
+
+console.log(employee)
+
+
+
+
+
+
+
+
+
+
